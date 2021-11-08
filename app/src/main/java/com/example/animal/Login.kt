92fbed2bc5ca1178dcb735aa60097529a457f092
DESKTOP-31K2ECJ\YuJaeMan <yooman22@naver.com>
2021-11-08 19:40:59 +0900

    유재만 : 회원 가입 화면 작업

--- app/src/main/java/com/example/animal/Login.kt
+++ app/src/main/java/com/example/animal/Login.kt
@@ -1,12 +1,21 @@
 package com.example.animal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        signUp.setOnClickListener{
+            val nextIntent = Intent(this, SignUp::class.java)
+            startActivity(nextIntent)
+        }
+
+
 
     }
 }
