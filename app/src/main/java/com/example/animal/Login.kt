9e1d8b57bc8718a65cd5ee2470514aca75dc018f
DESKTOP-31K2ECJ\YuJaeMan <yooman22@naver.com>
2021-11-08 22:02:30 +0900

    유재만 : 로그인 기능 추가

--- app/src/main/java/com/example/animal/Login.kt
+++ app/src/main/java/com/example/animal/Login.kt
@@ -1,14 +1,23 @@
 package com.example.animal
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        back_view.setOnClickListener{
+            var view = this.currentFocus
+            imm.hideSoftInputFromWindow(view?.windowToken,0)
+        }
 
         signUp.setOnClickListener{
             val nextIntent = Intent(this, SignUp::class.java)
