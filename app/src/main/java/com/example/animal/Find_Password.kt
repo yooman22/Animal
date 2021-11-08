9c243c8d884c2bf7df2b02339d6538fa2a87a131
DESKTOP-31K2ECJ\YuJaeMan <yooman22@naver.com>
2021-11-08 21:22:13 +0900

    새로운 화면 작업

---
+++ app/src/main/java/com/example/animal/Find_Password.kt
@@ -0,0 +1,11 @@
+package com.example.animal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Find_Password : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_password)
+    }
+}
